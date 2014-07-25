d0d3bdf6a36a43fae31270a08b35a92f2d2b5d60
elibtronic <tim.ribaric@gmail.com>
2014-07-25 11:22:05 -0400

    Scraper loops and writes to a file.

--- scrape.py
+++ scrape.py
@@ -1,17 +1,35 @@
 from bs4 import BeautifulSoup
+from time import sleep
 import urllib2,re
 
-bridges = [0,0,0,0,0,0,0,0]
 
-url = "http://greatlakes-seaway.com/R2/jsp/NiaBrdgStatus.jsp?language=E"
-needle = "Unavailable|Available"
-page = urllib2.urlopen(url)
-soup = BeautifulSoup(page)
+def check_bridges():
+    bridges = [0,0,0,0,0,0,0,0]
+    bstatus = open("status.txt","w")
+    url = "http://greatlakes-seaway.com/R2/jsp/NiaBrdgStatus.jsp?language=E"
+    needle = "Unavailable|Available"
+    page = urllib2.urlopen(url)
+    soup = BeautifulSoup(page)
 
-result = re.findall(needle, str(soup))
+    result = re.findall(needle, str(soup))
 
-for i in range(0,8):
-    if(result[i]=="Available"):
-        bridges[i] = 1
+    for i in range(0,8):
+        if(result[i]=="Available"):
+            bridges[i] = 1
+        
+    #print bridges
+
+    for i in range(0,8):
+        if (bridges[i] == 0):
+            bstatus.write("0")
+        else:
+            bstatus.write("1")
+    bstatus.close()
+
+
+if __name__ == "__main__":
+    while True:
+        print "Checking bridges...\n"
+        check_bridges()
+        sleep(30)
     
-print bridges
